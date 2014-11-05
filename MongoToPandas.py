d0d21129153bef739f49a6c6581f153a2b4f71a1
Zipfian Academy <Zipfian@Zipfians-Mac-mini-3.local>
2014-11-04 17:35:31 -0800

    First Commit

---
+++ MongoToPandas.py
@@ -0,0 +1,92 @@
+from pymongo import MongoClient
+import pandas as pd
+
+class MongoToPython():
+    '''
+    API for Mongo to Python
+    '''
+    
+    def __init__(self, database_name, host=None, post=None):
+        '''
+        INPUT: String, host=String, post=String
+        DESCRIPTION: initializes MongoClient and assigns the database
+        OUTPUT: None
+        '''
+        if host==None and post==None:
+            self.client = MongoClient()
+        else:
+            self.client = MongoClient(host, port)
+        self.db = self.client[database_name]
+
+    def query_all(self, collection_name, dict_requirements=None):
+        '''
+        INPUT: String, dict_requirements=String
+        DESCRIPTION: Generator for a Mongo Query
+        OUTPUT: Dictionary
+        '''
+        docs = self.db[collection_name]
+        if dict_requirements == None:
+            for doc in docs.find({}):
+                yield doc
+        else:
+            for doc in docs.find(dict_requirements):
+                yield doc
+
+    def mongo_to_lists(self, collection_name, column_names, dict_requirements=None):
+        '''
+        INPUT: String, List of Strings, dict_req=Dictionary
+        DESCRIPTION: Takes queries to a combined list or list
+        OUTPUT: List of Lists
+        '''
+        list_of_columns = []
+        for col in column_names:
+            list_of_columns.append([])
+        
+        for query in self.query_all(collection_name, dict_requirements):
+            for i in xrange(len(column_names)):
+                list_of_columns[i].append(query[column_names[i]])
+            
+        return list_of_columns
+
+    def mongo_to_df(self, collection_name, column_names, dict_requirements=dict_requirements):
+        '''
+        INPUT: String, List of Strings, dict_req=Dictionary
+        DESCRIPTION: Like mongo_to_lists but returns a dataframe
+        OUTPUT: pandas.DataFrame
+        '''
+        lst = self.mongo_to_lists(collection_name, column_names, dict_requirements=dict_requirements)
+        df = pd.DataFrame(data=lst)
+        df = df.T
+        df.columns = columns
+        return df
+
+    def query_for_each(self, collection_name, iterable, matching_field, columns):
+        '''
+        INPUT: String, iterable of strings, String, List 
+        DESCRIPTION: Does a query for each item
+        OUTPUT: Pandas DataFrame
+        '''
+        dicts_of_dicts = {}
+        
+        i = 0
+        for item in iterable:
+            for query in mongo.query_all(collection_name, dict_requirements={matching_field:item}):
+                dicts_of_dicts[i] = {'user_id': query['user_id'] , 'business_id': query['business_id'], 'rating':query['stars']}
+                i += 1
+        return pd.DataFrame(dicts_of_dicts).T
+    
+    def get_collection_keys(self, collection_name):
+        '''
+        INPUT: String
+        DESCRIPTION: Gets the keys for the first item in the mongodb WARNING!: is not gauranteed to get all keys in collection
+        OUTPUT: List 
+        '''
+        return self.db[collection_name].find_one({}).keys()
+    
+    def get_one(self, collection_name, key, value):
+        '''
+        INPUT: 
+        DESCRIPTION: 
+        OUTPUT:  
+        '''
+        return db[collection_name].find_one({key:value})
